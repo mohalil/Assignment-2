94652f6c51fc917e9d3c0602d856759d157f6ece
Mohammed Mohalil Naikwadi <80456603+mohalil@users.noreply.github.com>
2022-03-20 19:43:16 +0530

    Add files via upload

---
+++ Assignment-2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[]:
+
+
+# In[94]:
+
+
+list = [(2,5),(1,2),(4,4),(2,3),(2,1)]
+for i in range(len(list)):
+    for j in range(len(list)):
+        if list[i][1]<list[j][1]:
+            list[i],list[j]=list[j],list[i]
+print(list)
+
+# In[]:
+
+
+# In[6]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[]:
+
+
+# In[2]:
+
+
+d = {}
+for i in range(ord("a"),ord("z")+1):
+    d[chr(i)] = i
+print(d)
+
+
+
+# In[]:
+
+
+
+# In[ ]:
+
+
+
+
